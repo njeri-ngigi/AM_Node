fef65957bfae956c754a2156bc4f411e411cabbd
Njeri Ngigi <njery.ngigi@gmail.com>
2019-07-06 11:01:59 +0200

    add CORS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const ENV = require('./environment');
 const apiRouter = require('./routes');
+const cors = require('cors');
 
 const app = express();
-
 const port = ENV.PORT;
+const corsOptions = {
+  origin: 'http://localhost:4200'
+};
 
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use('/api/v1/am', apiRouter);
 
